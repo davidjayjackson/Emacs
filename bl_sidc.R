238012c813c6c3d9dd7f1e225954b68bbca4bbd1
davidjayjackson <davidjayjackson@gmail.com>
2019-06-12 14:11:54 -0400

    Updated header info.

--- bl_sidc.R
+++ bl_sidc.R
@@ -1,13 +1,12 @@
 ## Aurhor: Ben Letham (bletham@fb.com)
 ## Organization: Facebook
-## Package: Prophet Michine Learning
-## Data: http://sidc.be
+## Package: Prophet(Time Series)
+## Data: Solar Influences Data analysis Center(http://sidc.be)
 ## Date: 2019-05-31
 ## Purpose: To combine Sunspots data and R + Prophet(ML) to predict Sunspots/Miniumim activity.
-## Challenge: Was to take into account 11 year solor mim/max cycles
+## Challenge: Is  to take into account 11 year solor mim/max cycles
 ## Documentions: 
 ## https://facebook.github.io/prophet/docs/seasonality,_holiday_effects,_and_regressors.html#specifying-custom-seasonalities
-## My data import and cleaning code:
 ##
 library(data.table)
 library(ggplot2)
@@ -44,7 +43,7 @@ forecast <- as.data.table(forecast)
 ## Current Mininum: 2014 - 2019
 forecast1 <- forecast[ds >="2014-01-01",]
 ggplot(data=forecast1,aes(x=ds,y=yhat)) +geom_line() + geom_smooth() +
-  ggtitle("SIDC Current Mimimum: 2014 -2025")
+  ggtitle("SIDC Current Mimimum: 2014 -2022")
 ## 2019 prediction an plot
 forecast2 <- forecast[ds >="2019-01-01" & ds <="2021-05-31",]
 ggplot(data=forecast2,aes(x=ds,y=yhat)) +geom_line() + geom_smooth() +
